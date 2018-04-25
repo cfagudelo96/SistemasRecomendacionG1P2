b6c538df2e141bfd8ad8c267c377f045f81d7ff4
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-04-25 02:18:44 -0500

    Subir restaurantes

--- app/models/text_recommendation.rb
+++ app/models/text_recommendation.rb
@@ -6,6 +6,22 @@ class TextRecommendation < ApplicationRecord
 
   enum category: %i[restaurant bar nightlife]
 
+  def self.load_restaurants_recommendations
+    numero = 1000
+    while numero <= 73000
+      CSV.foreach("./data/recomendaciones_restaurants_#{numero}.csv", col_sep: ';', headers: false) do |row|
+        r = TextRecommendation.new(user_id: row[0],
+                                   business_id: row[1],
+                                   value: row[2].to_f,
+                                   category: :restaurant)
+        unless r.save
+          File.open('./data/restaurants_errors', 'a+') { |f| f << "#{row[0]};#{row[1]};#{row[2]}" }
+        end
+      end
+      numero += 1000
+    end
+  end
+
   def self.load_bars_recommendations
     CSV.foreach('./data/recomendaciones_bars.csv', col_sep: ';', headers: false) do |row|
       r = TextRecommendation.new(user_id: row[0],
